beaa253674fadf1baf1cdeca72d478faf8ab6176
Dharmesh49 <54776912+Dharmesh49@users.noreply.github.com>
2020-09-26 11:15:19 +0530

    First commit

---
+++ DharmeshShuklaProgramme/src/Dharmesh_shukla/DataType.java
@@ -0,0 +1,17 @@
+package Dharmesh_shukla;
+
+public class DataType {
+	
+	
+	public static void main(String args[]) {
+		
+	
+	System.out.println("Data Type");
+	
+	
+	
+	
+	
+	}
+	
+}
---
+++ DharmeshShuklaProgramme/src/Dharmesh_shukla/HelloProgramme.java
@@ -0,0 +1,19 @@
+package Dharmesh_shukla;
+
+public class HelloProgramme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+		int value1 = 4 , value2 = 2 ;
+	
+		  int value3 = value2+value1;
+		
+		System.out.println(value3);
+		
+
+	}
+
+}
